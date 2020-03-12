353bcafd607653018de26d5c252f10d8e399f4e5
Aurelien <aurelienmorel.3835@gmail.com>
2020-03-12 16:29:49 +0100

    mise en place de faux texte en cour

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,62 @@
 <?php
 
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        DB::table('posts')->insert([
+            'user_id' => Str::random(10),
+            'post_date'
+            'post_content' => $faker->paragraph,
+            'post_title' => Str::random(10),
+            'post_status'=> Str::random(10),
+            'post_name'=> $faker->name,
+            'post_type'=> Str::random(10),
+            'post_category'=> Str::random(10),
+            
+        ]);
     }
 }
+
+
+
+
+
+
+
+
+
+
+// --------------origine----------------------
+//use Illuminate\Database\Seeder;
+
+//class DatabaseSeeder extends Seeder
+//{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+//    public function run()
+//    {
+//        $this->call(UsersTableSeeder::class);
+//    }
+//}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4469,6 +4469,7 @@ class ComposerStaticInit3653e9fe7d2daf79be9c2f1b691d7345
         'TijsVerkoyen\\CssToInlineStyles\\Css\\Property\\Property' => __DIR__ . '/..' . '/tijsverkoyen/css-to-inline-styles/src/Css/Property/Property.php',
         'TijsVerkoyen\\CssToInlineStyles\\Css\\Rule\\Processor' => __DIR__ . '/..' . '/tijsverkoyen/css-to-inline-styles/src/Css/Rule/Processor.php',
         'TijsVerkoyen\\CssToInlineStyles\\Css\\Rule\\Rule' => __DIR__ . '/..' . '/tijsverkoyen/css-to-inline-styles/src/Css/Rule/Rule.php',
+        'UsersTableSeeder' => __DIR__ . '/../..' . '/database/seeds/UsersTableSeeder.php',
         'Webmozart\\Assert\\Assert' => __DIR__ . '/..' . '/webmozart/assert/src/Assert.php',
         'Whoops\\Exception\\ErrorException' => __DIR__ . '/..' . '/filp/whoops/src/Whoops/Exception/ErrorException.php',
         'Whoops\\Exception\\Formatter' => __DIR__ . '/..' . '/filp/whoops/src/Whoops/Exception/Formatter.php',
