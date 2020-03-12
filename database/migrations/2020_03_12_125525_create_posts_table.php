6732e70b63d1401fea7762533b7e9025c79dbc8b
Aurelien <aurelienmorel.3835@gmail.com>
2020-03-12 15:26:37 +0100

    creation des bases de donnees post et contact

---
+++ database/migrations/2020_03_12_125525_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->timestamp('post_date');
+            $table->longText('post_content');
+            $table->text('post_title');
+            $table->string('post_status',20);
+            $table->string('post_name',200);
+            $table->string('post_type',20);
+            $table->string('post_category',200);
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
